3a4dc5ccb59d5fc10cecb79eff8eac5224f5766e
aymanshahin <shahinayman1221@gmail.com>
2024-06-27 16:25:46 -0400

    Routed all product pages in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,18 @@ import HomePage from "./pages/HomePage";
 import BuildSetupPage from "./pages/BuildSetupPage";
 import BuildPage from "./pages/BuildPage";
 
+import CaseProductsPage from "./pages/product-pages/CaseProductsPage";
+import CoolerProductsPage from "./pages/product-pages/CoolerProductsPage";
+import CPUProductsPage from "./pages/product-pages/CPUProductsPage";
+import GPUProductsPage from "./pages/product-pages/GPUProductsPage";
+import KeyboardProductsPage from "./pages/product-pages/KeyboardProductsPage";
+import MemoryProductsPage from "./pages/product-pages/MemoryProductsPage";
+import MonitorProductsPage from "./pages/product-pages/MonitorProductsPage";
+import MotherboardProductsPage from "./pages/product-pages/MotherboardProductsPage";
+import OSProductsPage from "./pages/product-pages/OSProductsPage";
+import PowerSupplyProductsPage from "./pages/product-pages/PowerSupplyProductsPage";
+import StorageProductsPage from "./pages/product-pages/StorageProductsPage";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +25,17 @@ function App() {
                 <Route path="/home" element={<HomePage/>}/>
                 <Route path="/setup" element={<BuildSetupPage/>}/>
                 <Route path="/build" element={<BuildPage/>}/>
+                <Route path="/build/case" element={<CaseProductsPage/>}/>
+                <Route path="/build/cooler" element={<CoolerProductsPage/>}/>
+                <Route path="/build/CPU" element={<CPUProductsPage/>}/>
+                <Route path="/build/GPU" element={<GPUProductsPage/>}/>
+                <Route path="/build/Keyboard" element={<KeyboardProductsPage/>}/>
+                <Route path="/build/Memory" element={<MemoryProductsPage/>}/>
+                <Route path="/build/Monitor" element={<MonitorProductsPage/>}/>
+                <Route path="/build/Motherboard" element={<MotherboardProductsPage/>}/>
+                <Route path="/build/OS" element={<OSProductsPage/>}/>
+                <Route path="/build/PowerSupply" element={<PowerSupplyProductsPage/>}/>
+                <Route path="/build/Storage" element={<StorageProductsPage/>}/>
             </Routes>
         </BrowserRouter>
     );
