f6ca4259ade430ca913ca7b2f9fd21e9fce2c792
aymanshahin <shahinayman1221@gmail.com>
2024-07-03 22:26:44 -0400

    Implemented Kafka Consumer service

---
+++ backend/src/main/java/com/app/service/kafka/KafkaConsumerService.java
@@ -0,0 +1,39 @@
+package com.app.service.kafka;
+
+import com.app.entity.model.*;
+import com.app.entity.model.PCPart;
+import com.app.service.db.DBUpdateService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
+public class KafkaConsumerService {
+
+    private DBUpdateService dbUpdateService;
+
+    public KafkaConsumerService(DBUpdateService dbUpdateService) {
+        this.dbUpdateService = dbUpdateService;
+    }
+
+    @KafkaListener(
+            topics = {
+                    "casePartsTopic",
+                    "coolerPartsTopic",
+                    "cpuPartsTopic",
+                    "gpuPartsTopic",
+                    "keyboardPartsTopic",
+                    "memoryPartsTopic",
+                    "monitorPartsTopic",
+                    "motherboardPartsTopic",
+                    "osPartsTopic",
+                    "powerSupplyPartsTopic",
+                    "storagePartsTopic"
+            },
+            groupId = "consumerGroup1"
+    )
+    public void topicListener(PCPart pcPart) throws SQLException {
+        ((Upsertable) pcPart).insertPart(dbUpdateService);
+    }
+}
