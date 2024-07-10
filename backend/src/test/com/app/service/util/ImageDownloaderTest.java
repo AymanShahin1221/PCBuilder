22b42dd6231347e042423d1178fee5670b6f1bc1
aymanshahin <shahinayman1221@gmail.com>
2024-07-10 04:11:06 -0400

    Created test cases for ImageDownloader.java

---
+++ backend/src/test/com/app/service/util/ImageDownloaderTest.java
@@ -0,0 +1,24 @@
+package com.app.service.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageDownloaderTest {
+
+    @BeforeAll
+    static void setUp() {
+//         new File(TEST_DIRECTORY + "\\" + TEST_CATEGORY).mkdirs();
+    }
+
+    @AfterEach
+    void tearDown() {
+//        File file = new File(TEST_DIRECTORY + "\\" + TEST_CATEGORY + "\\" + TEST_FILE_NAME);
+//        if (file.exists())
+//            file.delete();
+    }
+}
