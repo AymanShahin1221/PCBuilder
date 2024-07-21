2bc7fc18c68089ca48fdac9f0b1fd167fb70fbe1
aymanshahin <shahinayman1221@gmail.com>
2024-07-21 02:22:46 -0400

    Renamed PartImageConfig to PartImageServiceConfig

---
+++ backend/src/main/java/com/app/config/PartImageServiceConfig.java
@@ -0,0 +1,24 @@
+package com.app.config;
+
+import com.app.service.db.PartImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PartImageServiceConfig {
+
+    private final PartImageService partImageService;
+
+    @Autowired
+    public PartImageServiceConfig(PartImageService partImageService) {
+        this.partImageService = partImageService;
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> partImageService.updateImagesInTables();
+    }
+}
+
