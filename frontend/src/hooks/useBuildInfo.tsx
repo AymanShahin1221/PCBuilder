58edf731854e472ea8cc3ee845c8cbb11da4df73
aymanshahin <shahinayman1221@gmail.com>
2024-07-24 04:05:58 -0400

    Created hooks folder

---
+++ frontend/src/hooks/useBuildInfo.tsx
@@ -0,0 +1,38 @@
+import {useEffect, useState} from "react";
+
+function useBuildInfo(): {buildName: string, budget: string} {
+    const [buildName, setBuildName] = useState<string>('');
+    const [budget, setBudget] = useState<string>('');
+
+    function fetchBuildData() {
+        fetch("http://localhost:8081/api/getBuildData", {
+            method: "GET",
+            mode: "cors"
+        })
+            .then(response => {
+                if (!response.ok)
+                    throw new Error('Network response was not ok');
+
+                return response.json();
+            })
+            .then(data => {
+                setBuildName(data.buildName);
+                setBudget(data.budget === null ? " None" : " $" + data.budget.toLocaleString("en-us", {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2
+                }));
+
+            })
+            .catch(error => {
+                console.error('There was a problem with your fetch operation:', error);
+            });
+    }
+
+    useEffect(() => {
+        fetchBuildData();
+    }, []);
+
+    return { buildName, budget };
+}
+
+export default useBuildInfo;
