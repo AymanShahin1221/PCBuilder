24f385600a029ac9ae154aa4645ffa3bbf433494
aymanshahin <shahinayman1221@gmail.com>
2024-07-24 04:06:30 -0400

    Refactored api call logic to be its own hook

--- frontend/src/components/BuildPageComponents/BuildInfo.tsx
+++ frontend/src/components/BuildPageComponents/BuildInfo.tsx
@@ -1,34 +1,8 @@
-import React, { useEffect, useState } from "react";
+import useBuildInfo from "../../hooks/useBuildInfo";
 
 function BuildInfo() {
 
-    const [buildName, setBuildName] = useState<string>('');
-    const [budget, setBudget] = useState<string>('');
-
-    function fetchBuildData() {
-        fetch("http://localhost:8081/api/getBuildData", {
-            method: "GET",
-            mode: "cors"
-        })
-            .then(response => {
-                if (!response.ok)
-                    throw new Error('Network response was not ok');
-
-                return response.json();
-            })
-            .then(data => {
-                setBuildName(data.buildName);
-                setBudget(data.budget === null ? " None" : " $" + data.budget.toLocaleString("en-us", { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
-
-            })
-            .catch(error => {
-                console.error('There was a problem with your fetch operation:', error);
-            });
-    }
-
-    useEffect(() => {
-        fetchBuildData();
-    }, []);
+    const {buildName, budget} = useBuildInfo();
 
     return (
         <div className="BuildPage_build-info-container container-fluid w-75 mt-4">
