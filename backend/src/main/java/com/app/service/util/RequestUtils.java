fa18f870e8dcca9477271fac092738acfefb14eb
aymanshahin <shahinayman1221@gmail.com>
2024-07-26 14:38:41 -0400

    Created Utility class for making requests

---
+++ backend/src/main/java/com/app/service/util/RequestUtils.java
@@ -0,0 +1,28 @@
+package com.app.service.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Utility class used for making HTTP requests
+ */
+
+public class RequestUtils {
+    private static final Logger logger = LoggerFactory.getLogger(RequestUtils.class);
+
+    public static ResponseEntity<String> makeGetRequest(String endpoint, HttpHeaders httpHeaders, RestTemplate restTemplate) {
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+        try
+        {
+            return restTemplate.exchange(endpoint, HttpMethod.GET, httpEntity, String.class);
+        }
+        catch (RestClientException rce)
+        {
+            logger.error("Failed to send HTTP request to {}", endpoint);
+            throw rce;
+        }
+    }
+}
