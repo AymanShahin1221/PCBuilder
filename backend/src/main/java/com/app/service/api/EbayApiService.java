43280286222a2c797a71e1a9a989464383c469df
aymanshahin <shahinayman1221@gmail.com>
2024-07-26 15:55:26 -0400

    Refactored test methods

--- backend/src/main/java/com/app/service/api/EbayApiService.java
+++ backend/src/main/java/com/app/service/api/EbayApiService.java
@@ -2,7 +2,7 @@ package com.app.service.api;
 
 import com.app.exception.InvalidApiResponseException;
 import com.app.exception.MaxCallsReachedException;
-import org.apache.kafka.common.metrics.stats.Max;
+import com.app.service.util.RequestUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import java.time.LocalTime;
 import com.app.service.util.JsonUtils;
@@ -24,7 +23,7 @@ import com.app.service.util.JsonUtils;
 @Qualifier("ebayApiService")
 public class EbayApiService implements ApiService {
 
-    private static final String API_TOKEN = "v^1.1#i^1#I^3#r^0#p^1#f^0#t^H4sIAAAAAAAAAOVYa2wUVRTe3W5BhEoCiFj8sQwaTWFn587sc9Jd3T6wCwtburUpFUPmcbcdO49l7h22izZpyiPBd8LrB0hAo0RMiPrDSCgQio/0BwSMiaCGkAgBGxNIo4FEI97ZlrKtBJBuYhP3z2bOPffc833nnHvPvUzPlGlVmxs2X69wTnXt7WF6XE4nmM5Mm1K+6JEyV2W5gylScO7tebLH3Vt2pRoJmprlmyDKGjqCni5N1RFfEEYpy9R5Q0AK4nVBg4jHEp+OL0/yLM3wWdPAhmSolCdRF6VE0e8HYVEGQIww/ghLpPotm81GlAoHRRiRg6EAkEVOAgIZR8iCCR1hQcdRimVYv5cJelm2mQnxXIDnAB2KsG2UpwWaSDF0okIzVKzgLl+Yaxb5endXBYSgiYkRKpaIL0mn4om6+hXN1b4iW7ERHtJYwBYa+1VryNDTIqgWvPsyqKDNpy1JgghRvtjwCmON8vFbzjyA+wWqZb9AKBQBGwrKQAKgJFQuMUxNwHf3w5YosjdTUOWhjhWcvxejhA3xZSjhka8VxESizmP/rbQEVcko0IxS9TXxVfHGRioWz2uCnu4QvI21NZaiytDb2FTnDcBASI7I4YCXEyWJBX5hZKFhayM0j1up1tBlxSYNeVYYuAYSr+F4btgibohSSk+Z8Qy2PSrWi9ziMBxus4M6HEULd+h2XKFGiPAUPu8dgdHZGJuKaGE4amH8QIGiKCVks4pMjR8s5OJI+nShKNWBcZb3+XK5HJ3jaMNs97EMA3yty5NpqQNqpNi6NLvWh/WVe0/wKgUoEiQzkcLjfJb40kVylTigt1MxfyQMAmCE97FuxcZL/yEowuwbWxGlqhABhDMRMZMhYWCg7C9JhcRGktRn+wFFIe/VBLMT4qwqSNArkTyzNGgqMjGXYblwBnrlYCTj9UcyGa8YkINekIGQgVAUpUj4/1Qo95vqaSiZEJck10uW5yvXt4s18fomsynPdsE6BbXAvK9e8LeFlq5btnZVetmq9XgtSoJgAkXvtxruCL5WVQgzzWT9UhBg13rpSGgwEIbyhOClJSMLGw1VkfKTK8CcKTcKJs6noaoSwYRAxrPZRGn26pLB+5fbxIPhLt0Z9R+dT3dEheyUnVyo7PmIGBCyCm2fQLRkaD671g2BtB+2eE3B6wnhVkjnOqlQE5DDaBV5uOWkC3BptE6iTYgMyyTdNp2yO7BmoxPq5DzDpqGq0GyZWAbY9axpFhZEFU62wi5BgivCJDtsQQiQqyMIcRPDJRWO0jWTbUsqxVbsfv4B22rf2Et+zFH4gV5nP9PrPOpyOplq5imwkFkwpewFd9mMSqRgSCtChkZKu07uriakO2E+Kyima7Zj6L3tDbWV9akdVa8050/v+sYxo+iNYe9LzLzRV4ZpZWB60ZMD88TtkXIw87EK0p4HWZYJcQEOtDELb4+6wVz3nHP7/ohx4KHq+ZtF5VDqh8FWru9npmJUyeksd7h7nY5HN+xJKAfOybt2Un3VA29/der0GwPW4orano+OHequGhjY3+g7PHTT813ZiZ4byZ3J1p/Y7jPah/KuVy/MO3t4w0VuS/8zn3Xntq0uP/77b8bnQ2eObnnc+X26Hi14jjs2c7G0g6u9HHh63eLkO/veki5vv2JtmQmuft19dNPSnPPSj1vxLDO/aOoHg9tSFxY5n7XK4jeGVr+5hzt4NZDqH/x41vsHXqt8ff4vZ6uvDc65KVoHXtxgQLVzfn9f1SPJk5s+9Xhav8xfaLrY29v/7vVK7fpW9/mHO37NHA9+ccq47NAWutiOrhNB18nzsw/u1uhv++b+tXvj3EsNFUeO/Ll/weyNV9RPruWGY/k3WT83jP0RAAA=";
+    private static final String API_TOKEN = "v^1.1#i^1#p^1#f^0#I^3#r^0#t^H4sIAAAAAAAAAOVYfWwURRS/u16LfBQMFCEFzbEF/qDZu9m92/vY9M5ce6U9vvpxpUBBy+zuXLuyt3vs7NEeGNPUCKQxaIwFxRjrB4FoEEMIiMZCBIMkkhA0oIQ/CAkIAU2UIJrwh7N3R7lWAkgvsYmXu1zmzZs37/eb92beDOgpGb9gc/3m26XWcbaBHtBjs1qZiWB8SXHl5CJbebEF5ClYB3rm9th7i65WYZhQknwzwklNxcjRnVBUzGeEQSqlq7wGsYx5FSYQ5g2Rj4WXLuFZJ+CTumZooqZQjmgkSAksEuN+TwC4yY/jBCJV79ps0YIU4jycFyDgBYIYB24v6cc4haIqNqBqBCkWsB4a+GjW28IEePIFnDPg5tooRyvSsaypRMUJqFDGXT4zVs/z9cGuQoyRbhAjVCgaXhhrCEcjtctaqlx5tkI5HmIGNFJ4eKtGk5CjFSop9OBpcEabj6VEEWFMuULZGYYb5cN3nXkM9zNUxzmRZRESPF4oIA/0F4TKhZqegMaD/TAlskTHM6o8Ug3ZSD+MUcKG8AISjVxrGTERjTjMv6YUVOS4jPQgVVsdXhVubKRC4XQCqrFOSDfWVKdkRUJ0Y3OE5hDnkwKSn6PdgiiyjAfmJspay9E8YqYaTZVkkzTsWKYZ1Yh4jUZyA/K4IUoNaoMejhumR3l6LHOXQzbQZi5qdhVTRqdqritKECIcmebDV2BotGHospAy0JCFkR0ZioIUTCZliRrZmYnFXPh04yDVaRhJ3uXq6upydrmdmt7hYgFgXCuXLomJnSgBKaJr5npWX374AFrOQBERGYll3kgniS/dJFaJA2oHFfIE/AzH5Hgf7lZopPQfgjzMruEZUagM8QDEMhKH3ALr9fj8vkJkSCgXpC7TDyTANJ2A+jpkJBUoIlokcZZKIF2WeDcXZ93+OKIlbyBOewLxOC1wkpdm4ggBkraCGPD/nxLlUUM9hkQdGQWJ9YLFedPGDqE6XNusN6fZbhSRcStKu2qhp823aMPi9atii1dtNNbjJYw3ioOPmg33BV+jyISZFjJ/IQgwc71wJNRr2EDSqODFRC2JGjVFFtNja4HdutQIdSMdQ4pCBKMCGU4mo4XZqwsG719uE4+Hu3Bn1H90Pt0XFTZDdmyhMsdjYgAmZad5AjlFLeEyc12DpPwwxe0Zr0eFWyaV65hCTUBm0cpStuR0ZuA68QbRqSOspXRSbTsbzAqsRVuHVHKeGbqmKEhvHV0EmPmcSKQMKChorCV2AQJchmPssGV8LKkmGHJRHBUuMXOUto+1LakQW7G97jHLatfwS37IkvkwvdavQa910Ga1giowj6kAc0qKltuLJpVj2UBOGcadWO5Qyd1VR851KJ2Esm6bZvn9g/76mvLahu0LNrWkT79zwjIp741h4Dkwc+iVYXwRMzHvyQHMvtdTzEyZUcp6gI/1MgEmALg2UHGv1848ZS87MHPb/M3n5AtXmNLpl6puTqnb+vwgKB1SslqLLfZeqyXEDYIdwatKEzev+KMtu8rqLz+796D30MXTZ26VRibMOPpmZcWJeTeeObn/zq/flYgvbXnVve/AT1PL1ff7/oKvh6TBM1XbbecDx/Yws44cvL7r1Ny+prLAFPXa7sovSo58svbKW4fTdfJv1Bs7L02tWDHh1p5J0yKT7yyCP1x/u7Su7NDOxLSL2yqalz992/+NyzJndXDr0tnhoqPHv5ou72+oexeePXyZGffa7v4vv+/rVz4+vGf+y5uOv/dk8c+TX3EvWnGh9/ztG+1PHKXP/FG52nFtHHXpw1Mv2o7dPP/L5wP2mkM/8t8iR1/TZ38O7Fw7cWV7fO+aSJfl2oJ+YcfJc2cHts7SbkT2fbrGkV3LvwGFkfMc/REAAA==";
     private final RestTemplate restTemplate;
 
     private static final String BASE_URL = "https://open.api.ebay.com/shopping?";
@@ -48,14 +47,25 @@ public class EbayApiService implements ApiService {
         return headers;
     }
 
+    private String constructApiEndpoint(String searchTerm) {
+        String encodedSearchTerm = searchTerm.replace(' ', '+');
+
+        return BASE_URL + "callname=FindProducts&" +
+                          "responseencoding=JSON&" +
+                          "siteid=0&" +
+                          "version=967&" +
+                          "QueryKeywords=" + encodedSearchTerm + "&" +
+                          "AvailableItemsOnly=false&" +
+                          "MaxEntries=1&";
+    }
+
     /**
      *
      * @param jsonObject json response object
-     * @param keywords keywords search
      * @return a boolean representing whether the JSON response is valid
      * @throws MaxCallsReachedException if the response indicates max call limit has been exceeded
      */
-    private boolean validateJSONResponse(JSONObject jsonObject, String keywords) throws MaxCallsReachedException, InvalidApiResponseException {
+    private boolean validateJSONResponse(JSONObject jsonObject) throws MaxCallsReachedException, InvalidApiResponseException {
 
         if(jsonObject.has("Errors"))
         {
@@ -64,10 +74,8 @@ public class EbayApiService implements ApiService {
             JSONObject error = errors.getJSONObject(0);
 
             if (error.getString("ShortMessage").equals("No match found."))
-            {
-                logger.info("No match found for {}", keywords);
                 return false;
-            }
+
             else if (error.getString("ShortMessage").equals("IP limit exceeded."))
                 throw new MaxCallsReachedException("Max api calls reached.");
 
@@ -80,49 +88,34 @@ public class EbayApiService implements ApiService {
 
     /**
      * Returns a string url of a product image
-     * @param keywords search term for product
+     * @param searchTerm search term for product
      */
-    public String getImgUrl(String keywords) throws MaxCallsReachedException, InvalidApiResponseException {
+    public String getImgUrl(String searchTerm) throws MaxCallsReachedException, InvalidApiResponseException {
 
-        String encodedKeywords = keywords.replace(' ', '&');
+        logger.info("Fetching image url for \"{}\"", searchTerm);
 
-        String endpoint = BASE_URL + "callname=FindProducts&" +
-                                     "responseencoding=JSON&" +
-                                     "siteid=0&" +
-                                     "version=967&" +
-                                     "QueryKeywords=" + encodedKeywords + "&" +
-                                     "AvailableItemsOnly=false&" +
-                                     "MaxEntries=1&";
-
+        String apiEndpoint = constructApiEndpoint(searchTerm);
         HttpHeaders headers = createHeadersForRequest();
-        HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        try
+        ResponseEntity<String> response = RequestUtils.makeGetRequest(apiEndpoint, headers, restTemplate);
+        if (response.getStatusCode().is2xxSuccessful())
         {
-            logger.info("Fetching image url...");
-
-            ResponseEntity<String> response = restTemplate.exchange(endpoint, HttpMethod.GET, entity, String.class);
-            if (response.getStatusCode().is2xxSuccessful())
+            JSONObject jsonData = JsonUtils.stringToJsonObject(response.getBody());
+            if(!validateJSONResponse(jsonData))
             {
-                JSONObject jsonData = JsonUtils.stringToJsonObject(response.getBody());
-
-                if(!validateJSONResponse(jsonData, keywords))
-                    return null;
-
-                JSONArray products = jsonData.getJSONArray("Product");
-                JSONObject product = products.getJSONObject(0);
-                boolean hasImage = product.getBoolean("DisplayStockPhotos");
-
-                if(hasImage)
-                {
-                    logger.info("Image URL found for keywords {}", keywords);
-                    return product.getString("StockPhotoURL");
-                }
+                logger.info("No match found for \"{}\"", searchTerm);
+                return null;
             }
-        }
-        catch (RestClientException e)
-        {
-            logger.error("Failed to send HTTP request.");
+
+            JSONArray products = jsonData.getJSONArray("Product");
+            JSONObject product = products.getJSONObject(0);
+            boolean hasImage = product.getBoolean("DisplayStockPhotos");
+
+            if(hasImage)
+            {
+                logger.info("Image URL found for search term \"{}\"", searchTerm);
+                return product.getString("StockPhotoURL");
+            }
         }
         return null;
     }
--- backend/src/test/com/app/service/api/EbayApiServiceTest.java
+++ backend/src/test/com/app/service/api/EbayApiServiceTest.java
@@ -12,31 +12,30 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-class EbayApiServiceTest {
+public class EbayApiServiceTest {
 
     @Autowired
     private EbayApiService ebayApiService;
 
-    // Note: Expected URLs are subject to change. Set expected URLs accordingly.
-    private static List<Arguments> provideMockKeywords() {
+    private static List<Arguments> provideMockSearchTerms() {
         return List.of(
                 Arguments.of("Intel core i9 14900k", "https://i.ebayimg.com/images/g/QQIAAOSwIAdlTLdC/s-l1600.jpg"),
                 Arguments.of("AMD Ryzen 9 5900X Desktop Processor", "https://i.ebayimg.com/images/g/u00AAOSwIIRj4LJj/s-l640.jpg"),
                 Arguments.of("AMD Ryzen 9 7950X", "https://i.ebayimg.com/images/g/s3oAAOSwaEpj5fNU/s-l960.jpg"),
                 Arguments.of("Intel Xeon E5-1660 V3", "https://i.ebayimg.com/images/g/cGsAAOSwIcRhbw4u/s-l640.jpg"),
-                Arguments.of("Intel Pentium G840", null),
+                Arguments.of("Intel Pentium G840", "https://i.ebayimg.com/images/g/XdwAAOxysE5SbMgt/s-l640.jpg"),
                 Arguments.of("Intel Core i9 15900k", null)
         );
     }
 
     @ParameterizedTest
-    @MethodSource("provideMockKeywords")
-    void testGetImgUrl(String mockKeywords, String expectedImgURL) throws MaxCallsReachedException, InvalidApiResponseException {
-        String actualImgUrl = ebayApiService.getImgUrl(mockKeywords);
+    @MethodSource("provideMockSearchTerms")
+    void testGetImgUrl(String mockSearchTerm, String expectedImgURL) throws MaxCallsReachedException, InvalidApiResponseException {
+        String actualImgUrl = ebayApiService.getImgUrl(mockSearchTerm);
         assertEquals(expectedImgURL, actualImgUrl);
     }
 }
