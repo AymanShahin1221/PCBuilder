63107ed5ce919643302e3eddb1cc65f650089d05
aymanshahin <shahinayman1221@gmail.com>
2024-07-28 17:17:46 -0400

    Optimized imports

--- backend/src/main/java/com/app/repository/GenericRepository.java
+++ backend/src/main/java/com/app/repository/GenericRepository.java
@@ -7,7 +7,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.*;
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
