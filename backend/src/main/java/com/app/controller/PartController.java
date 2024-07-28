547c66bc8745f0bc5ea45fc6693f90cbea6fec16
aymanshahin <shahinayman1221@gmail.com>
2024-07-28 17:10:56 -0400

    Refactored methods in PartController and filtered pid from json data

--- backend/src/main/java/com/app/controller/PartController.java
+++ backend/src/main/java/com/app/controller/PartController.java
@@ -8,42 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import static com.app.controller.PartCategory.*;
+import static com.app.service.util.DBUtils.getClassInstance;
 
 @RestController
 public class PartController {
     private final PartService partService;
 
-    private final Map<PartCategory, Class<? extends PCPart>> categoryClassMap = new HashMap<>();
-
     @Autowired
     public PartController(PartService partService) {
         this.partService = partService;
-
-        categoryClassMap.put(CASE, Case.class);
-        categoryClassMap.put(COOLER, Cooler.class);
-        categoryClassMap.put(CPU, CPU.class);
-        categoryClassMap.put(GPU, GPU.class);
-        categoryClassMap.put(KEYBOARD, Keyboard.class);
-        categoryClassMap.put(MEMORY, Memory.class);
-        categoryClassMap.put(MONITOR, Monitor.class);
-        categoryClassMap.put(MOTHERBOARD, Motherboard.class);
-        categoryClassMap.put(OS, OS.class);
-        categoryClassMap.put(POWER_SUPPLY, PowerSupply.class);
-        categoryClassMap.put(STORAGE, Storage.class);
-    }
-
-    private <T extends PCPart> Class<T> getClassInstance(String categoryName) {
-        return (Class<T>) categoryClassMap.get(PartCategory.valueOf(categoryName.toUpperCase()));
     }
 
     @GetMapping("/api/v1/getAllParts/{categoryName}")
     public <T extends PCPart> String getAllParts(@PathVariable("categoryName") String categoryName) {
         Class<T> partClass = getClassInstance(categoryName);
-        JSONArray jsonData =  partService.getAllPartsByCategory(partClass);
+        JSONArray jsonData =  partService.getAllPartsByCategory(partClass, List.of("pid"));
 
         return jsonData.toString();
     }
