ae24ca4ed89568a0b631a09c96fc3002959ade26
aymanshahin <shahinayman1221@gmail.com>
2024-07-28 17:11:34 -0400

    Moved enum from controller to util

---
+++ backend/src/main/java/com/app/service/util/PartCategory.java
@@ -0,0 +1,15 @@
+package com.app.service.util;
+
+public enum PartCategory {
+    CASE,
+    COOLER,
+    CPU,
+    GPU,
+    KEYBOARD,
+    MEMORY,
+    MONITOR,
+    MOTHERBOARD,
+    OS,
+    POWER_SUPPLY,
+    STORAGE
+}
