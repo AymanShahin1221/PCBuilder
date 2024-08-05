7284f77e6492a0590bf106f7a0ced92b830c7ee0
aymanshahin <shahinayman1221@gmail.com>
2024-08-05 16:53:54 -0400

    Renamed FilterByPriceSlider.tsx to CurrentBuildInfo.tsx

---
+++ frontend/src/components/ProductPageComponents/common/CurrentBuildInfo.tsx
@@ -0,0 +1,11 @@
+function CurrentBuildInfo() {
+    return (
+        <div className="info-container container col-sm-12 col-md-6 col-lg-2 d-flex flex-column align-items-center">
+            <div className="info-title-container">
+                <h2>Current Build</h2>
+            </div>
+        </div>
+    );
+}
+
+export default CurrentBuildInfo;
