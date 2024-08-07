15bcf8867c35713b0ec11b6aeeb6915fc9737434
aymanshahin <shahinayman1221@gmail.com>
2024-08-07 03:46:21 -0400

    Added dollar sign to budget in BuildInfo.tsx

--- frontend/src/components/BuildPageComponents/BuildInfo.tsx
+++ frontend/src/components/BuildPageComponents/BuildInfo.tsx
@@ -8,7 +8,7 @@ function BuildInfo() {
         <div className="BuildPage_build-info-container container-fluid w-75 mt-4">
             <div className="d-flex flex-row justify-content-start py-3">
                 <h3 className="me-5">{buildName}</h3>
-                <h3 className="ms-4">Budget:<span className="BuildPage_budget-header">{budget}</span></h3>
+                <h3 className="ms-4">Budget: <span className="BuildPage_budget-header">${budget}</span></h3>
             </div>
         </div>
     );
