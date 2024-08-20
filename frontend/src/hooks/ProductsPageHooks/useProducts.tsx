3d6c1a53c37cd3fac862a55d2690f5ad6783e9ff
aymanshahin <shahinayman1221@gmail.com>
2024-08-20 02:53:00 -0400

    Made current page persist throughout page re-render

--- frontend/src/hooks/ProductsPageHooks/useProducts.tsx
+++ frontend/src/hooks/ProductsPageHooks/useProducts.tsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function useProducts(category: string) {
-
     const [productsData, setProductsData] = useState({"totalEntries": 0, "products": []});
-    const [currentPage, setCurrentPage] = useState(0);
+
+    const [currentPage, setCurrentPage] = useState(() => {
+        const savedPage = localStorage.getItem("currentPage_" + category);
+        return savedPage ? parseInt(savedPage) : 0;
+    });
+
     const [loading, setLoading] = useState(false);
     const [entriesPerPage] = useState(30);
     const [totalEntries, setTotalEntries] = useState(0);
@@ -26,7 +30,7 @@ function useProducts(category: string) {
             const response = await axios.get(endpoint);
 
             setProductsData(response.data);
-            setTotalEntries(response.data["totalEntries"])
+            setTotalEntries(response.data["totalEntries"]);
             setLoading(false);
         }
         catch (error)
@@ -38,6 +42,10 @@ function useProducts(category: string) {
     useEffect(() => {
         fetchProducts();
     }, [currentPage]);
+
+    useEffect(() => {
+        localStorage.setItem("currentPage_" + category, currentPage.toString());
+    }, [currentPage, category]);
 
     return {
         productsData,
